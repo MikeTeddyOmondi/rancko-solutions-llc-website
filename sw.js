6abfa3e54c040d5df7fdc674194669e340dada1b
MikeTeddyOmondi <mike_omondi@outlook.com>
2021-07-16 16:10:41 +0300

    Updates commit

--- sw.js
+++ sw.js
@@ -1,44 +1,39 @@
-const cacheName = 'rancko-solutions-v1';
+const cacheName = "rancko-solutions-v1";
 
-const staticAssets = [
-    './',
-    './js/script.js',
-    './css/styles.css',
-    './fallback.json'
-];
+const staticAssets = ["./", "./js", "./css", "./images", "./fallback.json"];
 
-self.addEventListener('install', async function() {
-    const cache = await caches.open(cacheName);
-    cache.addAll(staticAssets);
+self.addEventListener("install", async function () {
+	const cache = await caches.open(cacheName);
+	cache.addAll(staticAssets);
 });
 
-self.addEventListener('activate', event => {
-    event.waitUntil(self.clients.claim());
+self.addEventListener("activate", (event) => {
+	event.waitUntil(self.clients.claim());
 });
 
-self.addEventListener('fetch', event => {
-    const request = event.request;
-    const url = new URL(request.url);
-    if (url.origin === location.origin) {
-        event.respondWith(cacheFirst(request));
-    } else {
-        event.respondWith(networkFirst(request));
-    }
+self.addEventListener("fetch", (event) => {
+	const request = event.request;
+	const url = new URL(request.url);
+	if (url.origin === location.origin) {
+		event.respondWith(cacheFirst(request));
+	} else {
+		event.respondWith(networkFirst(request));
+	}
 });
 
 async function cacheFirst(request) {
-    const cachedResponse = await caches.match(request);
-    return cachedResponse || fetch(request);
+	const cachedResponse = await caches.match(request);
+	return cachedResponse || fetch(request);
 }
 
 async function networkFirst(request) {
-    const dynamicCache = await caches.open('rancko-solutions-dynamic');
-    try {
-        const networkResponse = await fetch(request);
-        dynamicCache.put(request, networkResponse.clone());
-        return networkResponse;
-    } catch (err) {
-        const cachedResponse = await dynamicCache.match(request);
-        return cachedResponse || await caches.match('./fallback.json');
-    }
+	const dynamicCache = await caches.open("rancko-solutions-dynamic");
+	try {
+		const networkResponse = await fetch(request);
+		dynamicCache.put(request, networkResponse.clone());
+		return networkResponse;
+	} catch (err) {
+		const cachedResponse = await dynamicCache.match(request);
+		return cachedResponse || (await caches.match("./fallback.json"));
+	}
 }
