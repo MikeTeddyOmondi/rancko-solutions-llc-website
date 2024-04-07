87721aea432f9d9062f75d5dd8e1a7f172fd1a8f
MikeTeddyOmondi <mike_omondi@outlook.com>
2024-04-07 22:08:02 +0300

    Updates: version 2.0

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,35 +1,34 @@
-
 const ContactForm = () => {
   return (
     <div>
-      <form name='contactForm' action='' id='contactForm' novalidate>
-        <div class='row'>
-          <div class='input50'>
+      <form name='contactForm' action='' id='contactForm' noValidate>
+        <div className='row'>
+          <div className='input50'>
             <input type='text' name='fname' placeholder='First name' />
           </div>
-          <div class='input50'>
+          <div className='input50'>
             <input type='text' name='lname' placeholder='Last name' />
           </div>
         </div>
-        <div class='row'>
-          <div class='input50'>
+        <div className='row'>
+          <div className='input50'>
             <input type='email' name='email' placeholder='Email' />
           </div>
-          <div class='input50'>
+          <div className='input50'>
             <input type='text' name='subject' placeholder='Subject Title' />
           </div>
         </div>
-        <div class='row'>
-          <div class='input100'>
+        <div className='row'>
+          <div className='input100'>
             <textarea
               name='message'
               placeholder='Type in your message here ...'
             ></textarea>
           </div>
         </div>
-        <div class='row'>
-          <div class='input100'>
-            <input type='submit' class='btn' value='Send Message...' />
+        <div className='row'>
+          <div className='input100'>
+            <input type='submit' className='btn' value='Send Message...' />
           </div>
         </div>
       </form>
